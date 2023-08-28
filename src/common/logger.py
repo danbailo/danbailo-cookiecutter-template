891d0fd4fb8a1d41470db946948a6cbceb67f2eb
Daniel Bailo <danbailoufms@gmail.com>
2023-08-27 22:20:38 -0300

    feat: improved logger config

--- src/common/logger.py
+++ src/common/logger.py
@@ -1,9 +1,27 @@
 import logging
-import logging.config
 
-logging.config.fileConfig(
-    fname='./common/file.conf',
-    disable_existing_loggers=True
+import os
+
+# TODO: set alias as "local", "prod", "debug", etc.
+logger_level = os.environ.get('LOGGER_LEVEL') or 'DEBUG'
+
+MAPPED_LOGGER_LEVEL = {
+    'CRITICAL': logging.CRITICAL,
+    'FATAL': logging.FATAL,
+    'ERROR': logging.ERROR,
+    'WARNING': logging.WARNING,
+    'WARN': logging.WARN,
+    'INFO': logging.INFO,
+    'DEBUG': logging.DEBUG,
+    'NOTSET': logging.NOTSET,
+}
+
+logging.basicConfig(
+    format=(
+        '%(asctime)s - %(threadName)s - %(levelname)s - '
+        '%(filename)s:%(funcName)s:%(lineno)d - %(message)s'
+    )
 )
-# Get the logger specified in the file
+
 logger = logging.getLogger(__name__)
+logger.setLevel(MAPPED_LOGGER_LEVEL.get(logger_level))
