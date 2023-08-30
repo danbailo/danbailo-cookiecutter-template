e2f4286d2741d8b2fa12412510d55f74c8d10582
Daniel Bailo <danbailoufms@gmail.com>
2023-08-30 20:33:23 -0300

    feat: added common files

---
+++ src/common/decorators.py
@@ -0,0 +1,35 @@
+import asyncio
+
+from functools import wraps
+
+import time
+
+from .logger import logger
+
+
+def async_timeit(f):
+    @wraps(f)
+    def _async_timeit(*args, **kwargs):
+        start = time.monotonic()
+        try:
+            return asyncio.run(f(*args, **kwargs))
+        finally:
+            logger.info(
+                'time elapsed to run "%s" - %ss',
+                f.__name__, f'{time.monotonic() - start:0.2f}'
+            )
+    return _async_timeit
+
+
+def timeit(f):
+    @wraps(f)
+    def _timeit(*args, **kwargs):
+        start = time.monotonic()
+        try:
+            return f(*args, **kwargs)
+        finally:
+            logger.info(
+                'time elapsed to run "%s" - %ss',
+                f.__name__, f'{time.monotonic() - start:0.2f}'
+            )
+    return _timeit
---
+++ src/common/env_var.py
@@ -0,0 +1,25 @@
+import os
+
+from typing import Any
+
+from dotenv import load_dotenv
+
+from .logger import logger
+
+load_dotenv()
+
+ENV_VAR = {}
+
+
+def set_env_var(key: Any, value: Any) -> Any:
+    ENV_VAR[key] = value
+    return value
+
+
+def get_env_var(key: str, raise_exception: bool = True) -> str | None:
+    value = os.environ.get(key) or ENV_VAR.get(key)
+    if value is None and raise_exception is True:
+        raise ValueError(f'The variable "{key}" is not declared!')
+    if value is None and raise_exception is False:
+        logger.warning('variable is not declared - %s', key.upper())
+    return value
