ead08d728c72ca403f474e023f6862ba6c33ce31
Daniel Bailo <danbailoufms@gmail.com>
2023-10-10 10:25:47 -0300

    feat: added timeout decorator

--- src/common/decorators.py
+++ src/common/decorators.py
@@ -2,41 +2,66 @@ import asyncio
 
 from functools import wraps
 
+from math import ceil
+
+import signal
+
 import time
 
 from .logger import logger
 
 
-def coro(f):
-    @wraps(f)
+def coro(function):
+    @wraps(function)
     def wrapper(*args, **kwargs):
-        return asyncio.run(f(*args, **kwargs))
+        return asyncio.run(function(*args, **kwargs))
     return wrapper
 
 
-def async_timeit(f):
-    @wraps(f)
+def async_timeit(function):
+    @wraps(function)
     def _async_timeit(*args, **kwargs):
         start = time.monotonic()
         try:
-            return asyncio.run(f(*args, **kwargs))
+            return asyncio.run(function(*args, **kwargs))
         finally:
             logger.info(
-                f'time elapsed to run "{f.__name__}" - '
+                f'time elapsed to run "{function.__name__}" - '
                 f'{time.monotonic() - start:0.2f}s',
             )
     return _async_timeit
 
 
-def timeit(f):
-    @wraps(f)
+def timeit(function):
+    @wraps(function)
     def _timeit(*args, **kwargs):
         start = time.monotonic()
         try:
-            return f(*args, **kwargs)
+            return function(*args, **kwargs)
         finally:
             logger.info(
-                f'time elapsed to run "{f.__name__}" - '
+                f'time elapsed to run "{function.__name__}" - '
                 f'{time.monotonic() - start:0.2f}s',
             )
     return _timeit
+
+
+def timeout(seconds):
+    def decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            def handle_timeout(signum, frame):
+                raise TimeoutError(
+                    f'The function "{function.__name__}" is taking more than '
+                    f'{seconds} seconds to run! '
+                )
+
+            signal.signal(signal.SIGALRM, handle_timeout)
+            signal.alarm(ceil(seconds))
+
+            result = function(*args, **kwargs)
+
+            signal.alarm(0)
+            return result
+        return wrapper
+    return decorator
