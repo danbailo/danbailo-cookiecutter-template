56561597f770b21596cba1315c74872814ed0b2c
Daniel Bailo <danbailoufms@gmail.com>
2024-06-23 15:49:15 -0300

    Update requests.py

--- {{cookiecutter.repository_name}}/{{cookiecutter.repository_name|lower|replace("-", "_")}}/commons/requests.py
+++ {{cookiecutter.repository_name}}/{{cookiecutter.repository_name|lower|replace("-", "_")}}/commons/requests.py
@@ -38,6 +38,7 @@ async def make_async_request(
     timeout: int = 120,
     auth: tuple[str, str] | None = None,
     raise_for_status: bool = True,
+    follow_redirects: bool = True,
 ) -> Response:
     _logger.debug(
         'Making async request...',
@@ -52,6 +53,7 @@ async def make_async_request(
             files=files,
             timeout=timeout,
             auth=auth,
+            follow_redirects=follow_redirects,
         ),
     )
 
@@ -69,6 +71,7 @@ async def make_async_request(
             timeout=timeout,
             auth=auth,
             files=files,
+            follow_redirects=follow_redirects,
         )
     if raise_for_status:
         response.raise_for_status()
@@ -94,6 +97,7 @@ def make_request(
     timeout: int = 120,
     auth: Optional[tuple[str, str]] = None,
     raise_for_status: bool = True,
+    follow_redirects: bool = True,
 ) -> Response:
     _logger.debug(
         'Making request...',
@@ -108,6 +112,7 @@ def make_request(
             files=files,
             timeout=timeout,
             auth=auth,
+            follow_redirects=follow_redirects,
         ),
     )
 
@@ -124,6 +129,7 @@ def make_request(
         timeout=timeout,
         auth=auth,
         files=files,
+        follow_redirects=follow_redirects,
     )
     if raise_for_status:
         response.raise_for_status()
